29eb11f404fd770d980e40c39c3c601bb59f31ae
Rajibul Islam <rajibulski110@gmail.com>
2023-10-07 16:19:00 +0530

    Added documentation and removed limiter because it sucks

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ require("express-async-errors");
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
-const rateLimiter = require("express-rate-limit");
 
 const express = require("express");
 const app = express();
+
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 // connect DB
 const connectDB = require("./db/connect");
@@ -25,19 +29,15 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // extra packages
 app.set("trust proxy", 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-    legacyHeaders: false // Disable the `X-RateLimit-*` headers
-    // store: ... , // Use an external store for more precise rate limiting
-  })
-);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Jobs API</h1><a href="/api-docs">Documentation</a>`);
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
